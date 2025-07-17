e9c8c56a750b6bf9566d651a0b4d39bbf732b6cf
mehabalan <102800071+mehabalan@users.noreply.github.com>
2025-07-17 21:42:39 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+// Close menu when clicking the "×" inside
+document.querySelector(".menu-links").addEventListener("click", (e) => {
+  if (e.target.textContent === "×") {
+    document.querySelector(".menu-links").classList.remove("open");
+    document.querySelector(".hamburger-icon").classList.remove("open");
+  }
+});
